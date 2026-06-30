5e90060006c35e8853ce982d5c4dacae4f72e58a
unknown <clappertontee@gmail.com>
2026-06-30 10:51:04 +0200

    removed dublicates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Service from "../components/Services";
@@ -9,34 +8,6 @@ import CTA from "../components/CTA";
 export default function Home() {
 	return (
 		<>
-			<Helmet>
-				<title>
-					AFFMATT Mobile Mechanical Repairs | Mobile Mechanic Cape Town
-				</title>
-
-				<meta
-					name="description"
-					content="Mobile mechanic in Cape Town offering engine repairs, brake repairs, diagnostics, servicing and roadside assistance."
-				/>
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="AFFMATT Mobile Mechanical Repairs" />
-				<meta
-					property="og:description"
-					content="Trusted mobile mechanic in Cape Town for vehicle repairs, servicing and roadside assistance."
-				/>
-				<meta
-					property="og:image"
-					content="https://affmattmobilemechanical.co.za/og-image.jpg"
-				/>
-				<meta
-					property="og:url"
-					content="https://affmattmobilemechanical.co.za"
-				/>
-				<meta
-					name="twitter:description"
-					content="Trusted mobile mechanic in Cape Town for vehicle repairs, servicing and roadside assistance."
-				/>
-			</Helmet>
 			<Hero />
 			<Service />
 			<Features />
